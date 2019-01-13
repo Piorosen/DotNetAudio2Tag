f5b5d31371bc040bae780d99bd3b84291c02acd6
Piorosen <aoikazto@gmail.com>
2019-01-14 02:27:17 +0900

    NAudio Add

--- Tag.Core/CueSpliter.cs
+++ Tag.Core/CueSpliter.cs
@@ -8,7 +8,15 @@ namespace Tag.Core
 {
     public class CueSpliter
     {
-
+        public void Test()
+        {
+            ATL.CatalogDataReaders
+                .ICatalogDataReader reader = ATL.CatalogDataReaders
+                                                        .CatalogDataReaderFactory
+                                                        .GetInstance()
+                                                        .GetCatalogDataReader(@"D:\data\Coe\MYTH&ROID - HYDRA.cue");
+            ATL.Track track = reader.Tracks[0];
+        }
 
     }
 }
