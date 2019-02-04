4ae9434066d1adf6f8f9cf68a5e67b4c811de4ae
Piorosen <aoikazto@gmail.com>
2019-02-05 03:54:22 +0900

    프로젝트 자동 설정 프로그램 추가

--- ProjectSetting/Program.cs
+++ ProjectSetting/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace ProjectSetting
 {
     class Program
     {
+        static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+            
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+            
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var SlnDir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent;
+            var dir = SlnDir.FullName;
+
+            if (Directory.Exists(dir + @"\Tag.WPF\bin\Debug"))
+            {
+                Directory.Delete(dir + @"\Tag.WPF\bin\Debug", true);
+            }
+            else
+            {
+                Directory.CreateDirectory(dir + @"\Tag.WPF\bin\Debug");
+            }
+
+            DirectoryCopy(dir + @"\Tool", dir + @"\Tag.WPF\bin\Debug", true);
         }
     }
 }
