ee5f88313480d1c60e3b5bbc90bf2f988fd85bd9
Piorosen <aoikazto@gmail.com>
2019-02-06 06:11:08 +0900

    DialogHost 테스트 성공.

---
+++ Tag.WPF/Tagging/Design/Alarm.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tag.WPF
+{
+    /// <summary>
+    /// UserControl1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    /// 
+    public delegate void Result(object sender, string result);
+
+    public partial class Status : UserControl
+    {
+        public EventHandler<string> CloseEvent;
+        public void OnCloseEvent(string s)
+        {
+            CloseEvent?.Invoke(this, s);
+        }
+
+        public Status()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OnCloseEvent("YES");
+            
+        }
+    }
+}
--- Tag.WPF/Tagging/Tagging.xaml.cs
+++ Tag.WPF/Tagging/Tagging.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,8 +30,36 @@ namespace Tag.WPF
             DataContext = viewModel = new TaggingViewModel();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            var view = new Status
+            {
+
+            };
+            var result = await DialogHost.Show(view, ExtendedOpenedEventHandler, ExtendedClosingEventHandler);
+
+            //check the result...
+            Console.WriteLine("Dialog was closed, the CommandParameter used to close it was: " + (result ?? "NULL"));
+        }
+
+        private void ExtendedOpenedEventHandler(object sender, DialogOpenedEventArgs eventargs)
+        {
+            Console.WriteLine("You could intercept the open and affect the dialog using eventArgs.Session.");
+        }
+
+        private void ExtendedClosingEventHandler(object sender, DialogClosingEventArgs eventArgs)
+        {
+            if ((bool)eventArgs.Parameter == false) return;
+
+            //OK, lets cancel the close...
+            eventArgs.Cancel();
+            
+            //note, you can also grab the session when the dialog opens via the DialogOpenedEventHandler
+
+            //lets run a fake operation for 3 seconds then close this baby.
+            Task.Delay(TimeSpan.FromSeconds(3))
+                .ContinueWith((t, _) => eventArgs.Session.Close(false), null,
+                    TaskScheduler.FromCurrentSynchronizationContext());
 
         }
     }
