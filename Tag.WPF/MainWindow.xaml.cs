9d4bcba9c129bc4d3c2452b39ea7d1cff2c1ed91
Piorosen <aoikazto@gmail.com>
2019-02-06 12:23:41 +0900

    Cue 분리 디자인 적용

--- Tag.WPF/MainWindow.xaml.cs
+++ Tag.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,7 +26,7 @@ namespace Tag.WPF
         public MainWindow()
         {
             InitializeComponent();
-
+            new PaletteHelper().ReplacePrimaryColor("grey");
             Setting.Global.Language.Load("Kor.lang");
             DataContext = viewModel = new MainWindowsViewModel();
         }
