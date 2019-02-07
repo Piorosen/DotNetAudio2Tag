3da2649dd6355050fe1d0c0736532de96bf4d2f1
Piorosen <aoikazto@gmail.com>
2019-02-07 22:58:07 +0900

    Update MainWindow.xaml.cs

--- Tag.WPF/MainWindow.xaml.cs
+++ Tag.WPF/MainWindow.xaml.cs
@@ -16,19 +16,6 @@ namespace Tag.WPF
             new PaletteHelper().ReplacePrimaryColor("grey");
             
             DataContext = viewModel = new MainWindowsViewModel();
-
-            global::Tag.Core.Conv.AudioConverter conv = new Core.Conv.AudioConverter();
-            conv.AddFile(new Core.Conv.ConvInfo
-            {
-                FilePath = @"C:\Users\aoika\Desktop\임시\data\Coe\1. 1. MASAYUME CHASING.flac",
-                ResultPath = @"C:\Users\aoika\Desktop\임시\data\Coe\test.mp3",
-                Type = ATL.CatalogDataReaders.AudioType.FLAC
-
-            });
-            foreach (var i in conv.Execute())
-            {
-
-            }
         }
 
     }
