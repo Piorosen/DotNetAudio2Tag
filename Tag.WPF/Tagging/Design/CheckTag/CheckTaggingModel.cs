f5d84456251e38ef4e3ec38ba5a01f9fe11d3147
Piorosen <aoikazto@gmail.com>
2019-02-12 23:06:24 +0900

    태그 체크 파일 추가.

---
+++ Tag.WPF/Tagging/Design/CheckTag/CheckTaggingModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tag.WPF.Tagging.Design.CheckTag
+{
+    class CheckTaggingModel
+    {
+    }
+}
